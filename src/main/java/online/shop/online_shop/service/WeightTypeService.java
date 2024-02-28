caeb935daa10476cca504f02cdb0456502545916
Ozodbek <ozodbek007@gmail.com>
2024-02-28 10:55:38 +0500

    WeightTypeService checked

--- src/main/java/online/shop/online_shop/service/WeightTypeService.java
+++ src/main/java/online/shop/online_shop/service/WeightTypeService.java
@@ -17,9 +17,8 @@ public class WeightTypeService {
     }
 
     public ApiResponse<?> addWeightType(WeightTypeDto weightTypeDto) {
-        if (weightTypeRepository.existsByName(weightTypeDto.getName())) {
+        if (weightTypeRepository.existsByName(weightTypeDto.getName()))
             return new ApiResponse<>("Weight type already exists", false);
-        }
         WeightType weightType = new WeightType();
         weightType.setName(weightTypeDto.getName());
         weightTypeRepository.save(weightType);
@@ -27,23 +26,23 @@ public class WeightTypeService {
     }
 
     public ApiResponse<?> getWeightType(Long id){
-        WeightType weightType = weightTypeRepository.findById(id).orElseThrow(() ->
-                GenericNotFoundException.builder().message("Not found").statusCode(404).build());
-        return ApiResponse.builder().body(weightType).message("Success").success(true).build();
+        return ApiResponse.builder().body(getOneWeightType(id)).message("Success").success(true).build();
     }
 
     public ApiResponse<?> updateWeightType(WeightTypeDto weightTypeDto){
-        WeightType weightType = weightTypeRepository.findById(weightTypeDto.getId()).orElseThrow(() ->
-                GenericNotFoundException.builder().message("Not found").statusCode(404).build());
+        WeightType weightType = getOneWeightType(weightTypeDto.getId());
         weightType.setName(weightTypeDto.getName());
         weightTypeRepository.save(weightType);
         return new ApiResponse<>("Weight type updated", true);
     }
 
     public ApiResponse<?> deleteWeightType(Long id){
-        WeightType weightType = weightTypeRepository.findById(id).orElseThrow(() ->
-                GenericNotFoundException.builder().message("Not found").statusCode(404).build());
-        weightTypeRepository.delete(weightType);
+        weightTypeRepository.deleteById(id);
         return new ApiResponse<>("Weight type deleted", true);
+    }
+
+    public WeightType getOneWeightType(Long id){
+        return weightTypeRepository.findById(id).orElseThrow(() ->
+                GenericNotFoundException.builder().message("Not found").statusCode(404).build());
     }
 }
